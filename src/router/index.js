5d2a92e62f726db6ca1549f868cba5a67ef37c4d
pabpabpab <urallove@yandex.ru>
2022-03-19 16:56:36 +0500

    vue-router amendments
    vue-router amendments

--- src/router/index.js
+++ src/router/index.js
@@ -13,18 +13,12 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue'),
+    component: () => import('../views/LoginView.vue'),
   },
   {
     path: '/shop',
     name: 'shop',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ShopView.vue'),
+    component: () => import('../views/ShopView.vue'),
   },
 ];
 
